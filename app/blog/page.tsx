196b03ac7e2997abcd81dcbf37d5b30f13295ab9
reversed_R <qiu1287shan@gmail.com>
2024-12-01 11:52:06 +0900

    fix

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,13 +3,10 @@ import path from "path";
 import Blog from "./blog";
 import { ArticleMetadata } from "./ArticleMetadata.ts";
 
-type Params = {
-  articles: ArticleMetadata[];
-  tagTexts: string[];
-};
-
-export const generateStaticParams = (): Params => {
-  const data = fs.readFileSync(path.join(__dirname, "articles/meta/meta.json"));
+export const generateStaticParams = () => {
+  const data = fs.readFileSync(
+    path.join(process.cwd(), "app/blog/articles/meta/meta.json"),
+  );
 
   return JSON.parse(data.toString());
 };
